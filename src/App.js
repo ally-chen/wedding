e80986202aad89a22c7f56425d184492e39f5ded
Ally Chen/Home <allyroseline@gmail.com>
2021-12-04 02:41:32 +0800

    update form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import logo from './logo.svg';
 import cover from './images/cover.png';
 import boy from './images/boy.svg';
 import girl from './images/girl.svg';
-import { CoverWrapper, Figure, CardWrapper, CardContent, Title, TextDefault, TextDateTime, TextLocation, ChName, FormItem } from './style';
+import arrow from './images/arrow.svg';
+import { CoverWrapper, Figure, CardWrapper, CardContent, Title, TextDefault, TextDateTime, TextLocation, ChName,
+  FormWrapper, FormItem, ScrollLink, RadioGroup, RadioWrapper, BtnSubmit } from './style';
 import './App.css';
 
 function App() {
@@ -15,7 +17,7 @@ function App() {
         <CardContent>
           <Title>Lotus & Ally</Title>
           <Title>
-            <img src={boy} width="50" style={{ marginRight: 180 }} />
+            <img src={boy} width="50" />
             <img src={girl} width="50" />
           </Title>
           <TextDefault>Sincerely invite you to their wedding!</TextDefault>
@@ -23,8 +25,14 @@ function App() {
             2021 03 12<br />
             <small>Saturday 12:30</small>
           </TextDateTime>
-          <TextLocation>@晶宴民權館 第八劇場</TextLocation>
-          <form>
+          <TextLocation>
+            @晶宴民權館 第八劇場<br />
+            <small><a href="https://g.page/amazinghall_minquan?share" target="_blank" rel="noreferrer">台北市中山區民權東路三段2號B1</a></small>
+          </TextLocation>
+          <ScrollLink type="button">
+            <img src={arrow} alt="v" width="30" />
+          </ScrollLink>
+          <FormWrapper>
             <FormItem>
               <label>姓名</label>
               <input type="text" />
@@ -34,10 +42,81 @@ function App() {
               <input type="email" />
             </FormItem>
             <FormItem>
-              <label>出席狀況</label>
-              <input type="text" />
+              <label>身分</label>
+              <RadioGroup>
+                <RadioWrapper>
+                  <input type="radio" name="role" />
+                  <span>文翔親友</span>
+                </RadioWrapper>
+                <RadioWrapper>
+                  <input type="radio" name="role" />
+                  <span>怡君親友</span>
+                </RadioWrapper>
+              </RadioGroup>
             </FormItem>
-          </form>
+            <FormItem>
+              <label>出席狀況</label>
+              <RadioGroup>
+                <RadioWrapper>
+                  <input type="radio" name="attendance" />
+                  <span>我會出席</span>
+                </RadioWrapper>
+                <RadioWrapper>
+                  <input type="radio" name="attendance" />
+                  <span>隔空祝福</span>
+                </RadioWrapper>
+              </RadioGroup>
+            </FormItem>
+            <FormItem>
+              <label>出席人數</label>
+              <input type="number" min="1" placeholder="記得要算上自己唷!" />
+            </FormItem>
+            <FormItem>
+              <label>出席者1</label>
+              <RadioGroup>
+                <RadioWrapper>
+                  <input type="radio" name="food1" checked />
+                  <span>葷食</span>
+                </RadioWrapper>
+                <RadioWrapper>
+                  <input type="radio" name="food1" />
+                  <span>素食</span>
+                </RadioWrapper>
+              </RadioGroup>
+            </FormItem>
+            <FormItem>
+              <label>出席者2</label>
+              <RadioGroup>
+                <RadioWrapper>
+                  <input type="radio" name="food2" checked />
+                  <span>葷食</span>
+                </RadioWrapper>
+                <RadioWrapper>
+                  <input type="radio" name="food2" />
+                  <span>素食</span>
+                </RadioWrapper>
+              </RadioGroup>
+            </FormItem>
+            <FormItem>
+              <label>紙本喜帖</label>
+              <RadioGroup>
+                <RadioWrapper>
+                  <input type="radio" name="paper" />
+                  <span>請寄給我</span>
+                </RadioWrapper>
+                <RadioWrapper>
+                  <input type="radio" name="paper" />
+                  <span>我不需要</span>
+                </RadioWrapper>
+              </RadioGroup>
+            </FormItem>
+            <FormItem>
+              <label>想說的話</label>
+              <textarea rows="5"></textarea>
+            </FormItem>
+            <small>p.s. 當天女方不設禮金桌，有意送禮的朋友們可在領取喜餅時交予工作人員。</small>
+            <BtnSubmit type="submit">送 出</BtnSubmit>
+          </FormWrapper>
         </CardContent>
       </CardWrapper>
     </CoverWrapper>
--- src/style.js
+++ src/style.js
@@ -1,41 +1,55 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const CoverWrapper = styled.div`
-height: 100vh;
-min-height: 600px;
-padding: 0 20px;
+padding: 0 20px 20px;
 position: relative;
 &:after {
   content: '';
   display: block;
-  height: 100%;
-  top: 0;
-  width: 60%;
-  background: linear-gradient(to right, transparent 10%, #202020 30%);
+  width: 100%;
+  background: linear-gradient(to bottom, transparent 320px, #202020 400px);
   position: absolute;
-  right: 0;
+  top: 0;
+  bottom: 0;
+  left: 0;
   z-index: 1;
+}
+@media (min-width: 1024px) {
+  height: 100vh;
+  min-height: 600px;
+  padding-bottom: 0;
+  overflow: hidden;
+  &:after {
+    height: 100%;
+    top: 0;
+    background: linear-gradient(to right, transparent 10%, #202020 30%);
+    width: 60%;
+    right: 0;
+    left: auto;
+  }
 }
 `;
 
 export const Figure = styled.figure`
-height: 100%;
-width: 60%;
+height: 400px;
+width: calc(100% + 40px);
 background-size: cover;
 background-repeat: no-repeat;
-background-position: center;
-margin: 0 0 0 -20px;
+background-position: center 40%;
 position: relative;
+margin: 0 -20px;
+@media (min-width: 1024px) {
+  height: 100%;
+  width: 60%;
+  margin: 0 0 0 -20px;
+  background-position: center;
+}
 `;
 export const CardWrapper = styled.div`
-position: absolute;
-z-index: 2;
-right: calc(50vw - 600px);
-width: 600px;
-top: 10%;
-height: 80%;
 background-color: rgb(160 29 29 / 65%);
 text-align: center;
+position: relative;
+z-index: 2;
 &:before {
   content: '';
   position: absolute;
@@ -45,6 +59,14 @@ text-align: center;
   bottom: -8px;
   border: 2px solid rgb(160 29 29 / 65%);
   z-index: -1;
+}
+@media (min-width: 1024px) {
+  position: absolute;
+  left: 50%;
+  width: 45%;
+  min-width: 450px;
+  top: 10%;
+  height: 80%;
 }
 `;
 
@@ -68,62 +90,180 @@ overflow-y: scroll;
 export const Title = styled.div`
 font-weight: bold;
 font-family: 'Marck Script', serif;
-font-size: 72px;
+font-size: 36px;
+img + img {
+margin-left: 2.5em;
+margin-top: 10px;
+}
+@media (min-width: 1024px) {
+  font-size: 72px;
+}
 `;
 
 export const TextDefault = styled.div`
 
 `;
 export const TextDateTime = styled.div`
-font-size: 40px;
+font-size: 30px;
 margin: 0.75em 0;
 /* font-style: italic; */
 font-weight: bold;
 letter-spacing: 0.05em;
 line-height: 1;
 font-style: italic;
+@media (min-width: 1024px) {
+  font-size: 40px;
+}
 small{
-  font-size: 24px;
+  font-size: 0.6em;
   font-weight: 400;
 }
 `;
 export const TextLocation = styled.div`
-font-size: 16px;
+font-size: 14px;
+line-height: 1.5;
+@media (min-width: 1024px) {
+  font-size: 16px;
+}
 `;
 
 export const ChName = styled.figcaption`
-font-family: 'Zhi Mang Xing';
-font-size: 40px;
+font-family: 'Ma Shan Zheng';
+font-size: 28px;
 position: absolute;
-bottom: 2%;
-left:2%;
+bottom: 14%;
+right:2%;
 background: #000000a6;
 color: #fff;
 padding: 10px;
+@media (min-width: 1024px) {
+bottom: 2%;
+font-size: 40px;
+left: 2%;
+right: auto;
+}
+`;
+
+export const FormWrapper = styled.form`
+display:flex;
+flex-direction: column;
+align-items: center;
 `;
 
 export const FormItem = styled.div`
 display: flex;
 align-items: flex-start;
-width: 80%;
-margin: 0 auto 10px;
-> label {
-  margin-right: 20px;
-  width: 100px;
-  text-align: right;
-  line-height: 30px;
+flex-wrap: wrap;
+margin: 0 auto 20px;
+width: 100%;
+@media (min-width: 1024px) {
+  width: 80%;
 }
-> input {
+> label {
+  width: 100%;
+  text-align: left;
+  margin-bottom: 10px;
+  @media (min-width: 1024px) {
+    width: 80px;
+    text-align: right;
+    margin-right: 20px;
+    line-height: 30px;
+    margin-bottom: 0px;
+  }
+  + * {
+    width: 100%;
+    border-bottom: 1px solid #ffcba0;
+    @media (min-width: 1024px) {
+      width: calc(100% - 100px);
+      height: 30px;
+    }
+  }
+}
+input {
   border: 0;
   border-bottom: 1px solid #ffcba0;
   background-color: transparent;
   outline: none;
-  color: inherit;
+  color: #fff;
   font-family: inherit;
-  height: 30px;
   line-height: 29px;
-  font-size: 18px;
+  font-size: 16px;
   padding: 0 10px;
-  width: calc(80% - 100px);
+  text-align: center;
+  letter-spacing: 0.05em;
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.35);
+  }
+}
+textarea {
+  height: auto;
+  background-color: transparent;
+  border-color: #ffcba0;
+  outline: none;
+  color: #fff;
+  padding: 5px 10px;
+  line-height: 1.5em;
+  font-family: inherit;
+  letter-spacing: 0.05em;
+  font-size: 16px;
+}
+`;
+export const RadioGroup = styled.div`
+  display: flex;
+  justify-content: space-around;
+  line-height: 30px;
+`;
+export const RadioWrapper = styled.label`
+cursor: pointer;
+> input[type=radio] {
+  visibility: hidden;
+  position: absolute;
+  width: 1px;
+  &:checked + span {
+    opacity: 1;
+  }
+}
+> span {
+  opacity: 0.35;
+  color: #fff;
+  font-size: 16px;
+}
+`;
+const jumpping = keyframes`
+  0% {
+    transform: translate(0, 0);
+  }
+  100% {
+    transform: translate(0, -8px);
+  }
+`;
+
+export const ScrollLink = styled.button`
+font-size: 20px;
+background:none;
+border: none;
+outline: none;
+font-family: inherit;
+color: inherit;
+letter-spacing: 0.03em;
+margin: 60px 0 30px;
+> img {
+  animation: ${jumpping} 0.3s ease-out infinite alternate;
+}
+`;
+
+export const BtnSubmit = styled.button`
+border-radius: 30px;
+background: transparent;
+color: inherit;
+padding: 8px 24px;
+font-size: 16px;
+font-family: inherit;
+margin: 20px 0;
+border: 1px solid #ffcba0;
+cursor: pointer;
+&:hover {
+  color: #731e1e;
+  background: #ffcba0;
 }
 `;
